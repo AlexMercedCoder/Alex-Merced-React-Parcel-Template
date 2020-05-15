6af10e105840eaa6def27bcd1283f359c1d4ed54
Alex Merced <willcodeforfood@alexmerced.com>
2020-05-14 23:11:48 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header.js';
+import Footer from './components/Header.js';
+import './css/style.css';
+
+const App = (props) => {
+    return (
+        <>
+            <Header />
+            <h1>Hello World</h1>
+            <Footer />
+        </>
+    );
+};
+
+const target = document.getElementById('app');
+ReactDOM.render(<App />, target);
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default (props) => {
+    return (
+        <>
+            <h1>Footer</h1>
+        </>
+    );
+};
